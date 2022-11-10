38669c5b45b78ce4c6f57b1a3d60f9b7d33444fe
Alexander Sokolov <thorpe.nulled@gmail.com>
2022-11-10 21:53:01 +0300

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Home } from './components/pages/Home/Home';
+import { Profile } from './components/pages/Profile/Profile';
+import { Boards } from './components/pages/Boards/Boards';
+import { Board } from './components/pages/Board/Board';
+import { NotFound } from './components/pages/NotFound/NotFound';
+import PrivateRoutes from './utils/PrivateRoutes';
 
-function App() {
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/boards" element={<Boards />} />
+          <Route path="/board" element={<Board />} />
+        </Route>
+        <Route path="/404" element={<NotFound />} />
+        <Route path="/*" element={<Navigate to="/404" />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
-
-export default App;
+};
